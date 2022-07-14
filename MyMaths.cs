6307b5244df4d6a87271c1132de3b735bb432a11
khadija0401 <khadija.morbiwala1@gmail.com>
2022-07-14 09:21:30 +0530

    Console Project of String

---
+++ MyMaths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mylibrary
+{
+    public class MyMaths
+    {
+        public static void Add(int a,int b)
+        {
+            Console.WriteLine($"{a} + {b} = {(a + b)}");
+        }
+
+        public static void Sub(int a, int b)
+        {
+            Console.WriteLine($"{a} - {b} = {(a - b)}");
+        }
+
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine($"{a} * {b} = {(a * b)}");
+        }
+
+        public static void Divide(int a, int b)
+        {
+            Console.WriteLine($"{a} / {b} = {(a / b)}");
+        }
+
+     
+    }
+}
